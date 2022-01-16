70f51afd80d4235efeca02c91f3585ffcad79f3b
EC2 Default User <ec2-user@ip-172-31-33-107.ec2.internal>
2022-01-16 07:20:51 +0000

    9章9.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :email, presence: true, length: { maximum: 100 }, 
+                    format: { with: VALID_EMAIL_REGEX }, 
+                    uniqueness: true
+  validates :department, length: { in: 2..30 }, allow_blank: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
