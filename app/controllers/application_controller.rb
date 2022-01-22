802e3abde67df4981a9c2a2356c872ed3b98e14e
EC2 Default User <ec2-user@ip-172-31-33-107.ec2.internal>
2022-01-22 01:47:20 +0000

    10章10.4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,18 @@ class ApplicationController < ActionController::Base
   $days_of_the_week = %w{日 月 火 水 木 金 土}
   
   def set_one_month
-    @first_day = Date.current.beginning_of_month
+    @first_day = params[:date].nil? ? 
+    Date.current.beginning_of_month : params[:date].to_date
     @last_day = @first_day.end_of_month
     one_month = [*@first_day..@last_day]
-    @attendances = @user.attendances.where(worked_on: @first_day..@last_day)
+    
+    @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     
     unless one_month.count == @attendances.count
       ActiveRecord::Base.transaction do
         one_month.each { |day| @user.attendances.create!(worked_on: day) }
       end
+      @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     end
     
   rescue ActiveRecord::RecordInvalid
