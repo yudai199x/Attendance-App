1d11303492c52d77d2586b8ee6ba310f10744235
EC2 Default User <ec2-user@ip-172-31-33-107.ec2.internal>
2022-01-23 02:02:41 +0000

    10章10.6

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,2 +1,10 @@
 module AttendancesHelper
+  
+  def attendance_state(attendance)
+    if Date.current == attendance.worked_on
+      return '出社' if attendance.started_at.nil?
+      return '退社' if attendance.started_at.present? && attendance.finished_at.nil?
+    end
+    false
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20220118184151) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2022-01-15 23:00:00"
-    t.datetime "work_time", default: "2022-01-15 22:30:00"
+    t.datetime "basic_time", default: "2022-01-22 23:00:00"
+    t.datetime "work_time", default: "2022-01-22 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
