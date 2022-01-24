08df3f08737238a01762a34f7bb16270c64d6c4f
EC2 Default User <ec2-user@ip-172-31-33-107.ec2.internal>
2022-01-24 17:53:15 +0000

    Finish add-user-attendances

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -8,6 +8,10 @@ module AttendancesHelper
     return false
   end
   
+  def working_show(time, str)
+    return time.strftime(str) if time.present?
+  end
+  
   def working_times(start, finish)
     format("%.2f", (((finish - start) / 60) / 60.0))
   end
